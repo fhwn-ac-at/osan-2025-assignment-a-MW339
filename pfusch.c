d1dc769e3c5450d101934d23bdd371d7ea9bac88
Manuel <204222@fhwn.ac.at>
2025-04-10 10:15:11 +0200

    pfusch.c - getopt

--- pfusch.c
+++ pfusch.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <unistd.h>
+#include <stdbool.h>
 
-int main(void) {
-    printf("Hello, Kevin!\n");
+typedef struct command_line_arguments {
+  int i;
+  char const *s;
+  bool b;
+  } cli_args;
 
-    return 0;
+  cli_args parse_command_line(int const argc, char * argv [])
+{
+      cli_args args = {0, NULL, false};
+      int optgot = -1;
+do
+{
+  optgot = getopt(argc, argv, "i:s:b");
+  switch (optgot) {
+    case 'i':
+      args.i = atoi(optarg);
+    break;
+    case 's':
+      args.s = optarg;
+      break;
+      case 'b':
+        args.b = true;
+        break;
+        case '?':
+          printf("Usage: %s -i <number> -s <string> -b\n", argv[0]);
+          exit(EXIT_FAILURE);
+                 }
+} while (optgot != -1);
+    return args;
+    }
+
+int main(int argc, char * argv[]) {
+  cli_args const args = parse_command_line(argc, argv);
+
+  printf("i: %d, s: %s, b: %d\n", args.i, args.s, args.b);
 }
